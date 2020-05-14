933d729f75a5b504ac45a1e288b585c284e0a8db
Krista Rutz <rutzkri000@gmail.com>
2020-05-13 22:50:42 -0700

    add a forums page to nav bar and then follow tutorial to update root with custom features and styling

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as React from "react";
+import {
+  Platform,
+  StatusBar,
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  unstable_enableLogBox,
+} from "react-native";
 
-import useCachedResources from './hooks/useCachedResources';
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import LinkingConfiguration from './navigation/LinkingConfiguration';
+import useCachedResources from "./hooks/useCachedResources";
+import BottomTabNavigator from "./navigation/BottomTabNavigator";
+import LinkingConfiguration from "./navigation/LinkingConfiguration";
+
+import logo from "./assets/images/logo.png";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Stack = createStackNavigator();
 
@@ -16,13 +27,26 @@ export default function App(props) {
     return null;
   } else {
     return (
+      // <View style={styles.container}>
+      //   {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
+      //   <NavigationContainer linking={LinkingConfiguration}>
+      //     <Stack.Navigator>
+      //       <Stack.Screen name="Root" component={BottomTabNavigator} />
+      //     </Stack.Navigator>
+      //   </NavigationContainer>
+      // </View>
       <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
-        <NavigationContainer linking={LinkingConfiguration}>
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-          </Stack.Navigator>
-        </NavigationContainer>
+        <Image source={logo} style={styles.logo} />
+        <Text style={styles.message}>
+          To share a photo from your phone with a friend, just press the button
+          below!
+        </Text>
+        <TouchableOpacity
+          onPress={() => alert("Hello, world!")}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Pick a photo</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -31,6 +55,35 @@ export default function App(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  message: {
+    color: "#888",
+    fontSize: 18,
+    marginHorizontal: 22,
+    marginBottom: 10,
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "mediumseagreen",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "#fff",
   },
 });
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//   },
+// });
